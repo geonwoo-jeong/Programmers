23cfe7991cb88c46baa307a10e620cdc22c63950
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2018-12-11 11:59:42 +0900

    Create K번째수.js

---
+++ Sort/K번째수.js
@@ -0,0 +1,8 @@
+const solution = (array, commands) => {
+   return commands.map(command => 
+       array.slice(command[0] - 1, command[1]).sort((a, b) => a - b)[command[2] - 1]
+   )
+}
+
+solution([1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]);
+
